7d3ca4eaf543a9c4da8e4823eb24052d84a87f77
Arun-badode <arunbadode08@gmail.com>
2024-11-17 13:43:10 +0530

    i

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ arr.forEach(button => {
             string = string.substring(0, string.length-1);
             input.value = string;
         }
+    
 
        else{
         string += e.target.innerHTML;
